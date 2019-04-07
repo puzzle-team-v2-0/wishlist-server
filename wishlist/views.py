66e7f3cbb6de0903bff739d09544b3fc175130cb
Ivan Platonov <st049378@student.spbu.ru>
2019-04-07 23:53:42 +0300

    cs

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,5 +3,5 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-   queryset = User.objects.all()
-   serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
